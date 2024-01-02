6f0ed8f40f79bdcc6e82ab4c1a52bb071a0ba7dc
tomekkoziel <tomasz.koziel66@gmail.com>
2024-01-02 19:49:44 +0100

    staticfiles

--- navigation_app/settings.py
+++ navigation_app/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "navigation_app/static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "navigation_app/static",
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
