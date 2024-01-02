5843c0789dfbec25efe4dfbae052f4f09f71e4e1
tomekkoziel <tomasz.koziel66@gmail.com>
2024-01-02 20:05:08 +0100

    staticfiles

--- navigation_app/settings.py
+++ navigation_app/settings.py
@@ -28,7 +28,7 @@ CSRF_TRUSTED_ORIGINS = ["https://navigate-me-dc264612c23d.herokuapp.com/"]
 SECRET_KEY = "os.environ.get(SECRET_KEY)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
